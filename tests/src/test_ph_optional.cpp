98505b4f2d433d561f1d1dd15c818989c27a5a97
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-08-19 14:52:40 +0200

    a

---
+++ tests/src/test_ph_optional.cpp
@@ -0,0 +1,25 @@
+#include <catch2/catch.hpp>
+//#include <ph_concepts/concepts.hpp>
+#include <ph_file/file.hpp>
+
+using std::cout, std::endl;
+
+TEST_CASE ("main")
+{
+    
+    cout << "hi" << endl;
+}
+
+
+TEST_CASE ("read file")
+{
+    SECTION ("char const*")
+    {
+        op
+    }
+    
+    SECTION ("std::string")
+    {
+        
+    }
+}
